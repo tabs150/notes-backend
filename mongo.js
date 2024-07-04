137f3b4900f3d888fb50c6163534662166ef66d6
Thabiso Pheko <tabs150@gmail.com>
2024-07-04 12:56:49 +0200

    Fixed linting issues

--- mongo.js
+++ mongo.js
@@ -1,24 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log('give password as argument');
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://fullstack:${password}@fullstackopen.plczpjb.mongodb.net/noteApp?retryWrites=true&w=majority&appName=FullStackOpen`;
+const url = `mongodb+srv://fullstack:${password}@fullstackopen.plczpjb.mongodb.net/noteApp?retryWrites=true&w=majority&appName=FullStackOpen`
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false)
 
-mongoose.connect(url);
+mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
-});
+})
 
-const Note = mongoose.model('Note', noteSchema);
+const Note = mongoose.model('Note', noteSchema)
 
 // const note = new Note({
 //   content: 'Mongoose makes things easy',
@@ -32,7 +32,7 @@ const Note = mongoose.model('Note', noteSchema);
 
 Note.find({}).then((result) => {
   result.forEach((note) => {
-    console.log(note);
-  });
-  mongoose.connection.close();
-});
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
