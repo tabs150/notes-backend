eb6940903f242486c836628c2a686a0242781ec8
Thabiso Pheko <tabs150@gmail.com>
2024-07-04 13:58:09 +0200

    Change project structure

--- models/note.js
+++ models/note.js
@@ -1,17 +1,4 @@
-const mongoose = require('mongoose')
-
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+const mongoose = require('mongoose');
 
 const noteSchema = new mongoose.Schema({
   content: {
@@ -20,14 +7,14 @@ const noteSchema = new mongoose.Schema({
     required: true,
   },
   important: Boolean,
-})
+});
 
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   },
-})
+});
 
-module.exports = mongoose.model('Note', noteSchema)
+module.exports = mongoose.model('Note', noteSchema);
