efc6b99540b7c766383465a75a40fc0fdd27ee25
Thabiso Pheko <tabs150@gmail.com>
2024-07-08 17:22:25 +0200

    Eliminate the try-catch and use Promise.all

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -9,13 +9,16 @@ const Note = require('../models/note');
 
 beforeEach(async () => {
   await Note.deleteMany({});
-  let noteObject = new Note(helper.initialNotes[0]);
-  await noteObject.save();
-  noteObject = new Note(helper.initialNotes[1]);
-  await noteObject.save();
+  console.log('cleared');
+
+  const noteObjects = helper.initialNotes.map((note) => new Note(note));
+  const promiseArray = noteObjects.map((note) => note.save());
+  await Promise.all(promiseArray);
+  console.log('done');
 });
 
 test('notes are returned as json', async () => {
+  console.log('entered test');
   await api
     .get('/api/notes')
     .expect(200)
