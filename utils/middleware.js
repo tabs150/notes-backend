9ad82d52226734add32c50c2dff81bac6ff21cba
Thabiso Pheko <tabs150@gmail.com>
2024-07-12 17:39:58 +0200

    Token authentication - fix middleware token error

--- utils/middleware.js
+++ utils/middleware.js
@@ -26,8 +26,8 @@ const errorHandler = (error, request, response, next) => {
     return response
       .status(400)
       .json({ error: 'expected `username` to be unique' });
-  } else if (error.name === 'TokenExpiredError') {
-    return response.status(401).json({ error: 'token expired' });
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'token invalid' });
   }
 
   next(error);
