4354725c7ee28ada342f15959500f8849d90e784
Thabiso Pheko <tabs150@gmail.com>
2024-07-23 18:20:25 +0200

    E2E testing

--- utils/middleware.js
+++ utils/middleware.js
@@ -28,6 +28,8 @@ const errorHandler = (error, request, response, next) => {
       .json({ error: 'expected `username` to be unique' });
   } else if (error.name === 'JsonWebTokenError') {
     return response.status(401).json({ error: 'token invalid' });
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({ error: 'token expired' });
   }
 
   next(error);
